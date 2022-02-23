92e00ff7c6629439950a5c92c6d4eb7345e6c0d7
janealom084 <janealom084@gmail.com>
2022-02-23 18:07:15 +0600

    5000 users data showed in website

--- buddy.js
+++ buddy.js
@@ -1,5 +1,5 @@
 const loadBuddies = () => {
-    fetch('https://randomuser.me/api/?results=5')
+    fetch('https://randomuser.me/api/?results=5000')
         .then(Response => Response.json())
         .then(data => displyBuddies(data))
 }
@@ -10,8 +10,9 @@ const displyBuddies = data => {
     const buddies = data.results;
     const buddiesDiv = document.getElementById('buddies');
     for (const buddy of buddies) {
+        console.log(buddy);
         const p = document.createElement('p')
-        p.innerText = buddy.email;
+        p.innerText = `Name: ${buddy.name.title} ${buddy.name.first} ${buddy.name.last} Email: ${buddy.email} Photo: ${buddy.picture.medium} Address: ${buddy.location.city} ${buddy.location.country}`;
         buddiesDiv.appendChild(p);
     }
 }
