64905964fe94cb25d81c46b969cd1c694146fde1
janealom084 <janealom084@gmail.com>
2022-02-23 17:38:58 +0600

    data loded from server

--- buddy.js
+++ buddy.js
@@ -1,1 +1,15 @@
-console.log('hello js')
+const loadBuddies = () => {
+    fetch('https://randomuser.me/api/?results=5')
+        .then(Response => Response.json())
+        .then(data => displyBuddies(data))
+}
+
+loadBuddies();
+
+const displyBuddies = data => {
+    const buddies = data.results
+    for (const buddy of buddies) {
+        console.log(buddy);
+    }
+}
+
